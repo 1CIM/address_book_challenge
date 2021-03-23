01d33e1afb1fba472347476c8bcdb1e9c3c73393
kim_haaga <kim.haaga@live.se>
2021-03-23 22:10:44 +0100

    added test #2 for button and h2 text, added button and h2 to index.html

--- cypress/integration/userCanNavigateToApplication.feature.js
+++ cypress/integration/userCanNavigateToApplication.feature.js
@@ -1,6 +1,8 @@
 describe('Application', () => {
   it('is expected to display message', () => {
     cy.visit('/');
-    cy.get('h1').should('contain.text', 'Hello Yo');
+    cy.get('h1').should('contain.text', 'The Simple Address Book');
+    cy.get('h2').should('contain.text', 'Contacts');
+    cy.get('#add-contact').click()
   });
 });
